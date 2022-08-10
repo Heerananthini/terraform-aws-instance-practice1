005a6a44392051d1a109cade3c3fede33d340cdc
Heera <71284609+Heerananthini@users.noreply.github.com>
2022-08-10 09:46:02 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,11 +1,3 @@
-resource "aws_instance" "web" {
-  ami               = var.ami
-  instance_type     = var.instance_type
-  subnet_id = aws_subnet.web_subnet.id
-  tags              = {
-     "Name" = "web"
-  }
-}
 module "vpc-practice" {
   source  = "app.terraform.io/git_practice_ec2/vpc-practice/aws"
   version = "1.0.0"
@@ -13,4 +5,13 @@ module "vpc-practice" {
   vpc_cidr = var.vpc_cidr
   subnet_cidr = var.subnet_cidr
   available_zone = var.available_zone
+}
+
+  resource "aws_instance" "web" {
+  ami               = var.ami
+  instance_type     = var.instance_type
+  subnet_id = aws_subnet.web_subnet.id
+  tags              = {
+     "Name" = "web"
+  }
 }
