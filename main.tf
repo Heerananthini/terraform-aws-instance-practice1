d7ad2e5b29af81327cdbf76f90aa753ce34aefee
Heera <71284609+Heerananthini@users.noreply.github.com>
2022-08-10 09:42:22 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,11 +6,11 @@ resource "aws_instance" "web" {
      "Name" = "web"
   }
 }
-module "instance-practice" {
-  source  = "app.terraform.io/git_practice_ec2/instance-practice/aws"
+module "vpc-practice" {
+  source  = "app.terraform.io/git_practice_ec2/vpc-practice/aws"
   version = "1.0.0"
   # insert required variables here
-  ami = var.ami
-  instance_type = var.instance_type
-  
+  vpc_cidr = var.vpc_cidr
+  subnet_cidr = var.subnet_cidr
+  available_zone = var.available_zone
 }
